a6124aca1805fe2ee30afd776102b3aca692e17f
TeacherSpaceSchool <azyk.store@gmail.com>
2025-01-18 23:47:57 +0600

    check

--- module/clientAzyk.js
+++ module/clientAzyk.js
@@ -1,18 +1,3 @@
-const ClientAzyk = require('../models/clientAzyk');
-const UserAzyk = require('../models/userAzyk');
-const {deleteFile} = require('./const');
 
 module.exports.reductionToClient = async() => {
-    /*const jalalAbadClients = await ClientAzyk.find({city: 'Жалал-Абад'}).select('_id user image').lean()
-    const clientsForDelete = [], usersForDelete = []
-    for(let i=0; i<jalalAbadClients.length; i++){
-        clientsForDelete.push(jalalAbadClients[i]._id)
-        usersForDelete.push(jalalAbadClients[i].user)
-        if(jalalAbadClients[i].image) {
-            await deleteFile(jalalAbadClients[i].image)
-        }
-    }
-    await ClientAzyk.deleteMany({_id: {$in: clientsForDelete}})
-    await UserAzyk.deleteMany({_id: {$in: usersForDelete}})
-    console.log('reductionToClient: ', jalalAbadClients.length)*/
 }
