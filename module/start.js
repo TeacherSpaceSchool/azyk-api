783878cbfb2b0344802a636493ed52afc017b804
TeacherSpaceSchool <azyk.store@gmail.com>
2025-08-04 17:49:20 +0600

    check

--- module/start.js
+++ module/start.js
@@ -1,5 +1,8 @@
 const {createAdmin} = require('../module/user');
 const {Worker, isMainThread} = require('worker_threads');
+const InvoiceAzyk = require('../models/invoiceAzyk');
+const OrderAzyk = require('../models/orderAzyk');
+const SingleOutXMLAzyk = require('../models/singleOutXMLAzyk');
 
 let startDeleteBD = async () => {
     if(isMainThread) {
@@ -77,6 +80,33 @@ let start = async () => {
         await compactOldestDB()
         console.timeEnd('reduction DB')
    }, 10000)*/
+
+
+    //несинхронизованные заказы
+    const dateEnd = new Date()
+    dateEnd.setHours(3, 0, 0, 0)
+    dateEnd.setDate(dateEnd.getDate() + 1)
+    const dateStart = new Date(dateEnd)
+    dateStart.setDate(dateStart.getDate() - 2)
+    const unsynces = await InvoiceAzyk.find({
+        createdAt: {$gte: dateStart, $lt: dateEnd},
+        sync: {$nin: [1, 2]},
+        cancelClient: null,
+        cancelForwarder: null,
+        del: {$ne: 'deleted'},
+        taken: true,
+        organization: {$in: ['602ab46d3aa2070f708aa721', '6847cc17a838da40a7641fc8', '60367da5aea15a0f6def2e20']},
+    }).select('_id orders').lean()
+    let unsyncorders = [], unsyncinvoices = []
+    for(let i = 0; i<unsynces.length;i++) {
+        unsyncorders = [...unsyncorders, ...unsynces[i].orders]
+        unsyncinvoices = [...unsyncinvoices, unsynces[i]._id]
+    }
+    // eslint-disable-next-line no-undef
+    await Promise.all([
+        OrderAzyk.updateMany({_id: {$in: unsyncorders}}, {status: 'обработка'}),
+        InvoiceAzyk.updateMany({_id: {$in: unsyncinvoices}}, {taken: false, cancelClient: null, cancelForwarder: null})
+    ])
 }
 
 module.exports.start = start;
